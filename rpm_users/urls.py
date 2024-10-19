326c1b8b4525c9f5326871eeef7d2e54a6d487a8
Arshad <arshadaman202@gmail.com>
2024-10-19 20:11:26 +0530

    token

--- rpm_users/urls.py
+++ rpm_users/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path('add-patient/', views.create_patient),
+    path('patient-login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('patient-token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', views.moderator_login),
     path('update-patient/', views.update_patient),
     path('view-patient/<str:patient_id>/', views.moderator_actions_view, name='moderator_actions'),
