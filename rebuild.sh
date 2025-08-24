80ec4e8af5ba363b8fba43c711ca16523a83cd6e
Arshad Aman <38555984+ArshadAman@users.noreply.github.com>
2025-08-24 21:59:33 +0530

    rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,11 @@
 echo " ---- Redeploying RPM -----"
 echo " ---- Pulling latest changes from Git ----- "
-git pull origin dev
+if [ -z "$1" ]; then
+  echo "Error: No branch name provided."
+  echo "Usage: ./git-pull-branch.sh <branch-name>"
+  exit 1
+fi
+git pull origin "$1"
 echo " ---- Removing old images ----- "
 docker-compose down
 echo " ---- Building new images ----- "
